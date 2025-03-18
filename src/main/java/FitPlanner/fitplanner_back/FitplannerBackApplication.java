022fdc45f694ec78f03d8e25db4b6bf560e4dd6c
Ryan <ryannarelli12@gmail.com>
2025-03-17 23:32:48 -0300

    enviando arquivos

---
+++ src/main/java/FitPlanner/fitplanner_back/FitplannerBackApplication.java
@@ -0,0 +1,13 @@
+package FitPlanner.fitplanner_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitplannerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitplannerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FitPlanner/fitplanner_back/FitplannerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package FitPlanner.fitplanner_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitplannerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
